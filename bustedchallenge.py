f8349ec813ea2552af6bfa1b382dae1453534e8d
dephillips98 <dephillips5262@gmail.com>
2023-07-18 19:31:57 +0000

    join and split

---
+++ bustedchallenge.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+
+# Defined function
+def greet_user():
+    user_name = input("Please enter your name: ")
+    print("Hello, " + user_name + "!")
+    print("Welcome to Python class!")
+greet_user()    
---
+++ marvel_DC.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+heroes = [["Marvel",["Avengers",["Iron Man","Captain America","Thor"]],["X-Men",["Wolverine","Cyclops","Storm"]]],["DC Comics",["Justice League",["Superman","Batman","Wonder Woman"]],["Teen Titans",["Robin","Starfire","Raven", "Cyborg", "Beast Boy"]]]]
+print(f"My faveorite Avenger is {heroes[0][1][1][1]}")
+#print(f"My favorite X-Man is {heroes[X-Men[0]]}")
+#print(f"Here's a list of my favorite Teen Titans: {heroes[Teen Titans[2:5]]}")
