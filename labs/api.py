a998504644c2a626e2a90142cd08c6cd0700a605
dephillips98 <dephillips5262@gmail.com>
2023-07-24 15:04:58 +0000

    repush pokemon api

---
+++ labs/api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon"
+
+http_url = requests.get(url)
+results = http_url.json()
+#num = 0
+pokemon = results['results']
+#print(results["results"][0]["name"])
+#print(pokemon)
+def choice():
+    for num in range(2,20,3):
+        print(pokemon[num]["name"])
+
+
+    # TO DO
+    # prompt the user to choose three different pokemon
+    # send a .get() to all three of those pokemon
+    # process the response
+    # display what their type is
+
+choice()
