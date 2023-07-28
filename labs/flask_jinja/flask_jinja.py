502182ba06ceff8b0e01da98e9a11b35003fd315
dephillips98 <dephillips5262@gmail.com>
2023-07-28 21:21:19 +0000

    backup

---
+++ labs/flask_jinja/flask_jinja.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import render_template
+import requests
+
+def api():
+    url = 
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
