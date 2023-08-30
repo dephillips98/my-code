4aa4cec119a9b0ef2b56d9eb618e596dbbeea296
dephillips98 <dephillips5262@gmail.com>
2023-08-30 21:58:49 +0000

    update

--- labs/flask_jinja/flask_jinja.py
+++ labs/flask_jinja/flask_jinja.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import requests
 
-def api():
-    num = 1
-    for num in range(1,151): 
-        url = f"https://pokeapi.co/api/v2/pokemon-form/{num}"
-        response = requests.get(url)
-        data = response.json()
-        poke_name = data["name"]
-        poke_type = data["types"][0]["type"]["name"]
-        poke_pic = data["sprites"]["front_default"]
-        choose = input("search a pokemon by name:\n")
-        if poke_name == choose.lower():
-            return poke_name
-            return poke_type
-            return poke_pic
-        else:
-            print("try again")        
-
-
-
 app = Flask(__name__)
-@app.route("/<poke_name>")
-def homepage(poke_name):
-    api()
-    return render_template("poke.html", poke_name = poke_name)
+
+def get_pokemon_info(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
+    response = requests.get(url)
+    data = response.json()
+    poke_name = data["name"]
+    poke_type = data["types"][0]["type"]["name"]
+    return poke_name, poke_type
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    poke_name = ""
+    poke_type = ""
+
+    if request.method == "POST":
+        search_name = request.form.get("search_name")
+        if search_name:
+            poke_name, poke_type = get_pokemon_info(search_name)
+    
+    return render_template("poke.html", poke_name=poke_name, poke_type=poke_type)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224)
+
